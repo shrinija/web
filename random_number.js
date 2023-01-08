cfcafca2b3ca66dd23974956553d770d6a611d85
shrinija <98155154+shrinija@users.noreply.github.com>
2023-01-08 22:35:43 +0530

    Create random_number.js

---
+++ random_number.js
@@ -0,0 +1,22 @@
+//learn random number generation
+
+
+   var n=Math.random();
+
+-->random number that get generated is a       16 decimal place number      between  0 and 0.999999999999...              
+
+//....................................................................
+
+//if you are trying to do something like with dice game
+
+var ondice;
+ondice=Math.floor(Math.random()*6);
+console.log(ondice);
+
+//our range currently will be between 0 and 5    .... so....
+
+var ondice;
+ondice=Math.floor(Math.random()*6)+1;
+console.log(ondice);
+
+//.....................................................................
