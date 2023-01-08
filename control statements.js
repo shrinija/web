aba5e8559611f0982750b9309010b7676b524df4
shrinija <98155154+shrinija@users.noreply.github.com>
2023-01-08 23:46:05 +0530

    Create control statements.js

---
+++ control statements.js
@@ -0,0 +1,82 @@
+// learn loops 
+
+
+// ye    js   me thoda alg hai c++ se   function ke bahar ke variables bhi bina pass kiye andar use ho sakte....
+//.....parr andar ke bahar nai use kar sakte..function khatam honeke bad uska scope khatam
+
+
+//.........................................................................................................
+
+//while loop
+
+
+
+var output=[];
+var count=1;
+
+function fizzBuzz()
+{
+
+  while(count<=30)
+ {
+  if(count%3==0 && count%5==0)
+  {
+   output.push("fizzbuzz");
+  }
+  else if(count%3==0)
+  {
+    output.push("fizz");
+  }
+  else if(count%5==0)
+  {
+   output.push("buzz");
+  }
+  else{
+   output.push(count);
+  }
+  
+  count++;
+  
+}
+  
+}
+
+fizzBuzz()
+console.log(output);
+
+//....................................................................................................................
+
+// for loop
+
+var output=[];
+var count=1;
+
+function fizzBuzz()
+{
+
+for(;count<=30;count++)
+ {
+  if(count%3==0 && count%5==0)
+  {
+   output.push("fizzbuzz");
+  }
+  else if(count%3==0)
+  {
+    output.push("fizz");
+  }
+  else if(count%5==0)
+  {
+   output.push("buzz");
+  }
+  else{
+   output.push(count);
+  }
+  
+}
+  
+}
+
+fizzBuzz()
+console.log(output);
+
+//.......................................................................................................................
