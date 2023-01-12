361b47844b464a5450fe239041693bfaa714b36f
shrinija <98155154+shrinija@users.noreply.github.com>
2023-01-13 01:45:40 +0530

    Update dom.js

--- dom.js
+++ dom.js
@@ -57,8 +57,11 @@ var arr=document.getElementsByTagName("Li");
 
 
 //............
-document.querySelector("li a").style.color="pink";
-document.querySelector("li").style.color="blue";
+document.querySelector("li a").style.color="pink";     //more specific  ... this changed colour of that google link
+document.querySelector("li").style.color="blue";       // this changed only that dot(bullet point) color.. remember..
+document.querySelectorAll("li");   //as there are many list item
+
+document.querySelector(".list a").innerHTML="shrinija";
 //.............
 
 
@@ -76,7 +79,9 @@ document.querySelector("li").style.color="blue";
 document.querySelector("h1");
 document.querySelector("#title");
 document.querySelector(".btn"); 
+document.querySelector("#lists a");   // we are looking for an anchortag that is inside something that has an id of lists
 document.querySelector("li a");    // we are looking for a which is inside li
+document.querySelector("li.list");    //    here no space as li and list are in same line..        <li class="list"> <a href="https://www.google.com">Google</a> </li>
 
 
 -->when there are more than one elements having same class .. then only first one get selected
@@ -96,3 +101,49 @@ example    :        document.querySelector("h1").style.fontSize="10rem";
 
 //............................................................................................................................
 
+
+
+//for manipulating style keep this in mind ****************************************************************************
+
+//idea is -> HTML is for content only      CSS to style our website     JavaScript for behaviour   
+
+//but we was trying to change style of each element by js... which is not good practice
+
+document.querySelector("h1").style.color="blue";   // writing like this is not a good practice as we are changing property of element by js..
+-->but idealy all of our styles should be inside css
+
+//but if we want to change properties on fly.. we need js.. for that... 
+
+so... for that better way is...........
+
+-->we have a property         classList
+
+document.querySelector("button").classList;        // get list of all class 
+document.querySelector("button").classList.add("invisible");     // added invisible class
+document.querySelector("button").classList.remove("invisible");   // used to remove class
+document.querySelector("button").classList.toggle("invisible");    //if applied then remove if not then apply
+
+
+//...............................................................................................................................
+
+
+// for amnipulating text of HTML element************************************************************
+
+document.querySelector("h1").innerHTML;     // if we use inner HTML it will give all of the HTML including other HTML tag
+document.querySelector("h1").innerHTML="<em>shrinija</em>";               // so can to this also 
+document.querySelector("h1").textContent;     //gives only text
+
+
+//...............................................................................................................................
+
+//for manipulating attributes
+
+--> just keep in mind ....attribues are all the things that egt coured in atom
+(everything that goes inside tag .. other than tag name itself is an attribute)
+
+document.querySelector("a");
+document.querySelector("a").attributes;
+document.querySelector("a").getAttribute("href");
+document.querySelector("a").setAttribute("href","https://www.bing.com");
+
+//..............................................................................................................................
